f1b46fc0a9667d27f4bbb43b6ace2497c8157bfd
Nawodya Ishan <50957846+nawodyaishan@users.noreply.github.com>
2022-05-06 12:49:24 +0530

    Add Input System Add Chicken Controller

---
+++ Assets/Scripts/ChickenController.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Writing Main Script for Chicken Controller
+/// </summary>
+public class ChickenController : MonoBehaviour
+{
+    // Listen for movement keys and move
+    
+    //  Listen for space key and lay eggs
+}
